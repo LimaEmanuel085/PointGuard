844db484b8d1590f795590a5f4a96d35909f1d6f
Emanuel <limaemanuel773@gmail.com>
2025-08-30 19:03:04 -0300

    Finalização do CRUD de usuários

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -24,6 +24,21 @@ class userController extends Controller
         }
 
     }
+       public function findUserById($id)
+    {
+
+        try {
+            $user = User::findOrFail($id);
+
+            return response()->json(data: $user, status: 200);
+
+        } catch (Exception $e) {
+            // Lida com a exceção
+            return response()->json(data: ['error' => $e->getMessage()], status: 500);
+        }
+
+    }
+
 
     public function store(Request $request)
     {
@@ -37,11 +52,38 @@ class userController extends Controller
                 'total_hours_required' => $request->input('total_hours_required')
             ]));
 
-            return response()->json(data: $user, status: 200);
+            return response()->json(data: [$user, 'message' => 'Usuário criado com sucesso!'], status: 200);
 
         } catch (Exception $e) {
             return response()->json(data: ['error' => $e->getMessage()], status: 500);
         }
-
     }
+
+    public function destroy($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+
+            return response()->json(data: ['message' => 'Usuário deletado com sucesso!'], status: 200);
+
+        } catch (Exception $e) {
+            return response()->json(data: ['error' => $e->getMessage()], status: 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+
+            $user->update($request->only(['name', 'password', 'total_hours_required']));
+
+            return response()->json(data: ['message' => 'Usuário atualizado com sucesso!'], status: 200);
+
+        } catch (Exception $e) {
+            return response()->json(data: ['error' => $e->getMessage()], status: 500);
+        }
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\inputHoursController;
 
 Route::get('/users', [userController::class, 'index']);
 Route::post('/user', [userController::class, 'store']);
+Route::get('/user/{id}', [userController::class, 'findUserById']);
+Route::delete('/user/{id}', [userController::class, 'destroy']);
+Route::put('/user/{id}', [userController::class, 'update']);
 
 Route::get('/input-hours', [inputHoursController::class, 'index']);
